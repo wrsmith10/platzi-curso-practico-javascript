770a89646369b91a6730c0c3484407c93155887b
wrsmith10 <richardsmithortegon@gmail.com>
2021-10-03 11:14:20 -0500

    Funcionalidad de calcular perimetros y area de un cuadrado incorporada con elementos de HTML

--- figuras.js
+++ figuras.js
@@ -35,3 +35,20 @@ function AreaCirculo(radio){
     return radio*radio*PI;
 }
 console.groupEnd();
+
+function calcularPerimetroCuadrado(){
+    const input=document.getElementById("InputCuadrado");
+    const value=input.value;
+
+
+    const perimetro=perimetroCuadrado(value);
+    alert(perimetro);
+}
+function calcularAreaCuadrado(){
+    const input=document.getElementById("InputCuadrado");
+    const value=input.value;
+
+
+    const area=areaCuadrado(value);
+    alert(area);
+}
